1b34003e90e7e59d38a655e8c86a79879df53e27
Roger Campos <rogerandrecampos@outlook.com>
2026-05-31 20:18:27 -0300

    atualizando estado abandonado, simplificado nome dos estados

--- model/estado_midia.py
+++ model/estado_midia.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 # o estado contexto faz a troca dos estados e o repasse do que deve ser feito para o estado correto
 class Estado_Contexto:
     def __init__(self) -> None:
-        self._estado_atual = Estado_nao_iniciado()
+        self._estado_atual = Nao_iniciado()
 
     def set_estado(self, novo_estado):
         self._estado_atual = novo_estado
@@ -15,19 +15,14 @@ class Estado_Contexto:
         self._estado_atual.pausar(self)
     def concluir(self):
         self._estado_atual.concluir(self)
+    def abandonar(self):
+        self._estado_atual.abandonar(self)
+    def get_nome(self):
+        self._estado_atual.get_nome()
 
-class Estado_midia(ABC):
-    """     @property 
-    def midia(self):
-        return self.midia
-    
-    @midia.setter
-    def midia(self, atribuir_midia):
-        from model.midia import Midia
-        if not isinstance(atribuir_midia, Midia):
-            raise TypeError("Erro, objeto não é uma Midia")
-        self.midia = atribuir_midia """
-    
+
+
+class Estado_midia(ABC):    
     @abstractmethod
     def iniciar(self, midia):
         pass
@@ -36,6 +31,9 @@ class Estado_midia(ABC):
         pass
     @abstractmethod
     def concluir(self, midia):
+        pass
+    @abstractmethod
+    def abandonar(self, midia):
         pass
 
     @abstractmethod
@@ -52,23 +50,52 @@ class Estado_midia(ABC):
 
 #classes concretas de estados
 
-class Estado_nao_iniciado(Estado_midia):
+class Nao_iniciado(Estado_midia):
     def get_nome(self):
         return "Não Iniciado"
     
     def iniciar(self, midia):
-        midia.set_estado(Estado_em_progresso())
+        pass
+
+    def pausar(self, midia):
+        print("Não é possivel pausar, mídia não iniciada")
+
+    def concluir(self, midia):
+        midia.set_estado(Em_progresso())
+
+    def abandonar(self, midia):
+        midia.set_estado(Abandonado())
+
+class Em_progresso(Estado_midia):
+    def get_nome(self):
+        return "Em Progresso"
+    
+    def iniciar(self, midia):
+        pass
 
     def pausar(self, midia):
         pass
 
     def concluir(self, midia):
-        pass
+        midia.set_estado(Concluido())
 
-class Estado_em_progresso(Estado_midia):
-    def get_nome(self):
-        return "Em Progresso"
-
-class Estado_concluido(Estado_midia):
+class Concluido(Estado_midia):
     def get_nome(self):
         return "Concluído"
+
+class Pausa(Estado_midia):
+    def get_nome(self):
+        return "Em Pausa"
+
+class Abandonado(Estado_midia):
+    def get_nome(self):
+        return "Abandonado"
+    def iniciar(self, midia):
+        pass
+
+    def pausar(self, midia):
+        pass
+
+    def concluir(self, midia):
+        midia.set_estado(Concluido())
+    
