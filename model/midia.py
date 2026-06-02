972c0a3f517b86e5903f98b9979f106e64317579
Roger Campos <rogerandrecampos@outlook.com>
2026-06-02 09:57:05 -0300

    criadas classes filhas midia

--- model/midia.py
+++ model/midia.py
@@ -1,14 +1,14 @@
-#from abc import ABC, abstractmethod
+from abc import ABC, abstractmethod
 from model.Estado_midia import Estado_Contexto
+from datetime import datetime, timedelta
 
-class Midia():
+class Midia(ABC):
     def __init__(self, titulo: str):
         self.titulo = titulo
-        #self._tipo = tipo# alterar dps p tipo
         self.estado_atual = Estado_Contexto()
-        #self.data_inclusao: date
-        #self.data_atualizado: date
-        #self.genero: list[Genero] = []
+        self.data_inclusao: datetime
+        self.data_atualizado: datetime
+        #self.generos: list[Genero] = []
         #self.plataforma_acesso = list[Plataformas]
         self._comentario = None
     
@@ -40,13 +40,34 @@ class Midia():
             raise ValueError("O nome não pode ser vazio!")
         self.titulo = add_comment
 
-    """ def __eq__(self, outro:object) -> bool:
+    @abstractmethod
+    def __eq__(self, outro:object) -> bool:
+        pass
+        '''
         if not isinstance(outro, Midia):
             return False
-        return (self.titulo == outro.titulo 
-                and self.tipo == outro.tipo) """
-    
-    #@abstractmethod
+        return (self.titulo == outro.titulo)
+        '''
+        
+    @abstractmethod
     def __str__(self):
         return (f"{self.titulo}")
     
+class filmes(Midia):
+    def __init__(self, titulo: str):
+        super().__init__(titulo)
+class livros(Midia):
+    def __init__(self, titulo: str):
+        super().__init__(titulo)
+class Mangas(Midia):
+    def __init__(self, titulo: str):
+        super().__init__(titulo)
+class Animes(Midia):
+    def __init__(self, titulo: str):
+        super().__init__(titulo)
+class Seriados(Midia):
+    def __init__(self, titulo: str):
+        super().__init__(titulo)
+class Jogos(Midia):
+    def __init__(self, titulo: str):
+        super().__init__(titulo)
