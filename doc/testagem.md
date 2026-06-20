ce3abd916d8bcc760d5e801e5fb3688a6454dcf6
Roger Campos <rogerandrecampos@outlook.com>
2026-06-19 23:19:51 -0300

    testando usando pytest e venv

---
+++ doc/testagem.md
@@ -0,0 +1,13 @@
+#criando enviroment:
+python3 -m venv venv
+
+#ativando enviroment:
+###Windows:
+.\venv\Scripts\activate.bat
+###mac/Linux:
+source venv/bin/activate
+
+#modulo de testes pytest:
+pip install pytest
+
+depois pode usar a funcao de testes do vscode
--- test/test_models.py
+++ test/test_models.py
@@ -4,16 +4,18 @@ from abc import ABC, abstractmethod
 # Adiciona a pasta raiz do projeto
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from ..src.models.factory_midia import factory_Midia
+from src.models.factory_midia import factory_Midia
+from src.models.midia import Midia
 
 
-
-if __name__ == "__main__":
+def test_criacao_midia_factory():
     filme_teste = factory_Midia.nova_midia("filme 01", "filme")
 
-    print(f"{filme_teste}")
+    assert isinstance(filme_teste, Midia), "erro na criacao de midia"
+
+   """  print(f"{filme_teste}")
 
     filme_teste.estado.iniciar()
     filme_teste.estado.concluir()
 
-    print(filme_teste.__repr__())
+    print(filme_teste.__repr__()) """
